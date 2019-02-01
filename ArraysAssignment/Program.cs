7ac99c009d669ce338869e02f9a99aef893a991f
Chris Mejia <cbmejia@usf.edu>
2019-01-31 21:13:38 -0500

    Version 1

--- ArraysAssignment/Program.cs
+++ ArraysAssignment/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿/*
+ *  Author: Christopher Mejia
+ *  Date: 1/31/19
+ * 
+ */
+using System;
 
 namespace ArraysAssignment
 {
@@ -6,7 +11,28 @@ namespace ArraysAssignment
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+
+            try
+            {
+                int[] myValues = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25 };
+
+                for (int i = 0; i < myValues.Length; i++)
+                {
+                    Console.WriteLine("Element Value = " + myValues[i]);
+                    Console.WriteLine("The Element Subscript for this Value is " + i);
+                    Console.ReadKey(true);
+
+                }// end of for loop
+
+                Console.WriteLine("All values in the array have been displayed. Press any key to exit");
+                Console.ReadKey(true);
+            }// end of try
+
+            catch
+            {
+                Console.WriteLine("An Error ocurred! Press any key to exit.");
+                Console.ReadKey(true);
+            }// end of catch
+        }// end of main
+    }//end of class
+}// end of namespace
